89bcf5c6bf32408e582ed39fce1d92d422a851f4
hossien <hossien@DESKTOP-56K4TO3>
2024-12-24 14:30:07 +0330

    add migration and conncect to database

--- WebShopApp/Data/WebShopDBContext.cs
+++ WebShopApp/Data/WebShopDBContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebShopApp.Models;
 
 namespace WebShopApp.Data
 {
     public class WebShopDBContext:DbContext
     {
         public WebShopDBContext(DbContextOptions<WebShopDBContext> options) : base(options) { }
+        public DbSet<Product> products { get; set; }
     }
 }
--- WebShopApp/Program.cs
+++ WebShopApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<WebShopDBContext>(options =>
-options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConncection")));
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
